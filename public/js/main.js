cac491368b3117e2e78e1f7d4a374b5631a63567
angelnogueras <angelnogueras.git@gmail.com>
2019-06-10 16:32:35 +0200

    added clock and some styling

--- public/js/main.js
+++ public/js/main.js
@@ -23,6 +23,7 @@
     interGEO = setInterval( ()=> {
       getNetworkListGeo()
     }, 10 * 1000 )
+    chronoStart()
     btnStart.disabled = true
     btnStop.disabled = false
     chiquito.style.visibility = 'visible'
@@ -37,6 +38,7 @@
     started = false
     clearInterval(interIP)
     clearInterval(interGEO)
+    chronoStop()
     btnStart.disabled = false
     btnStop.disabled = true
     chiquito.style.visibility = 'hidden'
@@ -65,5 +67,62 @@
       .catch(error => console.error(error))
   }
 
+  // Chronometer
+  const chronoElem = document.getElementById('chrono')
+
+  let startTime = 0
+  let start = 0
+  let end = 0
+  let diff = 0
+  let timerID = 0
+
+  const chrono = () => {
+  	end = new Date()
+  	diff = end - start
+  	diff = new Date(diff)
+  	let msec = diff.getMilliseconds()
+  	let sec = diff.getSeconds()
+  	let min = diff.getMinutes()
+  	let hr = diff.getHours()-1
+  	if (min < 10){
+  		min = "0" + min
+  	}
+  	if (sec < 10){
+  		sec = "0" + sec
+  	}
+  	if(msec < 10){
+  		msec = "00" +msec
+  	}
+  	else if(msec < 100){
+  		msec = "0" +msec
+  	}
+  	chronoElem.innerHTML = hr + ":" + min + ":" + sec + ":" + msec
+  	timerID = setTimeout(chrono, 10)
+  }
+
+  const chronoStart = () => {
+  	start = new Date()
+  	chrono()
+  }
+
+  const chronoContinue = () => {
+  	start = new Date()-diff
+  	start = new Date(start)
+  	chrono()
+  }
+
+  const chronoReset = () => {
+  	chronoElem.innerHTML = "0:00:00:000"
+  	start = new Date()
+  }
+
+  const chronoStopReset = () => {
+  	chronoElem.innerHTML = "0:00:00:000"
+  }
+
+  const chronoStop = () => {
+  	clearTimeout(timerID)
+  }
+//-->
 
 })()
